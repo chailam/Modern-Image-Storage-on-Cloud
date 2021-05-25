a2ca1262f0aaa6d2989860a6c3e6ba9b2a756dcc
Chai Lam <cloi0001@student.monash.edu>
2021-05-26 05:18:51 +1000

    added upload-s3 lambda and updated API gateway txt; added upload image query lambda

---
+++ queries/upload_image_query_lambda/lambda_function.py
@@ -0,0 +1,69 @@
+import json
+import objectDetection as od
+from botocore.exceptions import ClientError
+import boto3
+import base64
+
+"""
+front end have to parse image to byte/string as demonstrated in assignment 1 client
+"""
+
+def lambda_handler(event, context):
+    theRequest =event['http-method']
+    
+    if (theRequest == 'POST'):
+        print("POST request")
+        try:
+            # Call object detection function
+            b64string = event['body-json']['based64string']
+            image_file = base64.b64decode(b64string)
+            result = od.objectDetect(image_file)
+            # print(output)
+            
+            labels = []
+            
+            for o in result:
+                labels.append(o[0])
+            #labels = ["laptop","keyboard"]
+                
+            # Remove duplicate at labels
+            labels = list(set(labels))
+                
+            client = boto3.resource("dynamodb")
+            table = client.Table("detectedImage")
+            
+            items = table.scan()['Items']
+            scanned_images = []
+        
+            for i in items:
+                tags = i['tag']
+                
+                # Check if tags consist of labels 
+                flag = True
+                for l in labels:
+                    if l not in tags:
+                        flag = False
+                        
+                if flag == True:
+                    scanned_images.append(i['url'])
+                    print(scanned_images)
+                
+            retrun_items = {"links" : scanned_images}
+        
+        except ClientError as e:
+            print (e)
+            # Return Error JSON or http error code
+            return {
+                "isBase64Encoded": False,
+                "statusCode": 400,
+                "headers": {"Content-Type": "application/json"},
+                "body": json.dumps(e)
+            }
+    
+        else:
+            return {
+                "isBase64Encoded": False,
+                "statusCode": 200,
+                "headers": {"Content-Type": "application/json"},
+                "body": json.dumps(retrun_items)
+            }
