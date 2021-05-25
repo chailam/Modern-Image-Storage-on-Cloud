211992e90d72a00610d2e94a12acb8d5099043bf
Chai Lam <cloi0001@student.monash.edu>
2021-05-26 05:23:33 +1000

    include lambda function code for upload image to s3

---
+++ upload_detect_image/upload_img_s3_from_POST_lambda/index.js
@@ -0,0 +1,44 @@
+// can only upload jpg file
+/*  Base64 encoded String for image is send in request body.
+*/
+
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+exports.handler = (event, context, callback) => {
+    let request = JSON.parse(event.body);
+    
+    // get the image
+    let encodeImage = request.based64string;
+    let decodeBufferImage = Buffer.from(encodeImage, 'base64');
+    
+  
+    // Construct a filepath
+    var filePath = context.awsRequestId + ".jpg";  // "mime/jpg"
+    
+    // Construct the parameter
+    var params = {
+      "Body": decodeBufferImage,
+      "Bucket": "fit5225-uploaded-image",
+      "Key": filePath,
+      "ContentType " :"mime/jpg"   //fileMime 
+      };
+      
+    // Upload image
+    s3.upload(params, function (err, data) {
+      // if error
+    if (err) {
+      console.log(err);
+      callback(err, null);
+    } else {
+      // if successful
+      console.log(data);
+      let response = {
+        "statusCode": 200,
+        "body": JSON.stringify(data),
+        "isBase64Encoded": false
+      };
+      callback(null, response);
+    }
+    });
+};
