5d89ba50c4ce61871f4f68af0e4328602d20fddd
Chai Lam <cloi0001@student.monash.edu>
2021-05-28 18:54:49 +1000

    modify the UI

--- upload_detect_image/upload_img_s3_from_POST_lambda/index.js
+++ upload_detect_image/upload_img_s3_from_POST_lambda/index.js
@@ -37,6 +37,7 @@ exports.handler = (event, context, callback) => {
       console.log(data);
       let response = {
         "statusCode": 200,
+        "headers":{"Access-Control-Allow-Origin":"*"},
         "body": JSON.stringify(data),
         "isBase64Encoded": false
       };
